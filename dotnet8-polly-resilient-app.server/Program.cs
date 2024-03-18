fd7963726ab0e98bccbf5e34a532a99ef815dda3
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2024-03-18 17:44:14 -0600

    Adding notes and Adding Client

--- dotnet8-polly-resilient-app.server/Program.cs
+++ dotnet8-polly-resilient-app.server/Program.cs
@@ -21,7 +21,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", async () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
@@ -32,9 +32,7 @@ app.MapGet("/weatherforecast", () =>
         ))
         .ToArray();
     return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+});
 
 app.Run();
 
